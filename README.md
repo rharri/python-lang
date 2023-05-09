979db38ca7270b441f735f101f2d607b3fc9708a
Ryan H <rharri@users.noreply.github.com>
2023-05-08 20:51:37 -0400

    Reorganize source tree

---
+++ README.md
@@ -0,0 +1,10 @@
+## Description
+Random Python language stuff.
+
+## Example Usage
+```
+$ python3 -m src.python_lang.super
+```
+
+## License
+See LICENSE
---
+++ src/python_lang/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+    print("Python Language")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
